9e2825435a3f8f7886599ee292b22aa04c74cd43
katriel <katrielaraujo@gmail.com>
2023-12-14 22:39:50 -0300

    Terminado

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o número da agência: ");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Digite o número da conta: ");
+        Integer numero = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Digite o nome do cliente: ");
+        String clienteNome = scanner.nextLine();
+
+        System.out.println("Digite o saldo da conta: ");
+        Double saldo = scanner.nextDouble();
+
+        System.out.printf("Olá %s obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.3f já está disponível para saque",clienteNome,agencia,numero,saldo);
     }
 }
